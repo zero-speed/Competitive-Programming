1a961fc2e166c1adaa2550d2af0c094394cb02f0
zero_speed <215733@unsaac.edu.pe>
2026-04-23 16:03:15 -0500

    update

--- note book/Basics/template.cpp
+++ note book/Basics/template.cpp
@@ -21,3 +21,19 @@ int main(){
 
     return 0;
 }
+
+/*
+LINUX / WSL / ICPC (RECOMENDADO)
+g++ -std=c++17 -O2 archivo.cpp -o archivo
+./archivo < in.txt > out.txt
+
+WINDOWS - CMD
+g++ -std=c++17 -O2 archivo.cpp -o archivo.exe
+archivo.exe < in.txt > out.txt
+
+WINDOWS - POWERSHELL
+g++ -std=c++17 -O2 archivo.cpp -o archivo.exe
+Get-Content in.txt | .\archivo.exe
+Get-Content in.txt | .\archivo.exe > out.txt
+.\archivo.exe < in.txt > out.txt
+*/
