157ae2f8c242fc240f769e262c231e343abfd15a
zero_speed <215733@unsaac.edu.pe>
2026-04-23 17:53:55 -0500

    update

--- note book/Data Structures/segtree.cpp
+++ note book/Data Structures/segtree.cpp
@@ -1,64 +1,80 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // complexity: O(n log n) for build, O(log n) for update and query
 using namespace std;
 
 struct segtree{
-    vector<int> arbol;
-    vector<int> valores;
+    vector<int> tree;
+    vector<int> lazy;
     int n;
     segtree(int n){
         this->n = n;
-        arbol.resize(4*n);
-        valores.resize(n);
+        tree.resize(4*n);
+        lazy.resize(4*n, 0);
     }
 
-    void init(int l, int r, int nodo){
-        if(l==r){
-            arbol[nodo] = valores[l];
+    void build(vector<int>& arr, int node, int l, int r){
+        if(l == r){
+            tree[node] = arr[l];
             return;
         }
-        int mid = (l+r)/2;
-        init(l, mid, 2*nodo);
-        init(mid+1, r, 2*nodo+1);
-        arbol[nodo] = arbol[2*nodo] + arbol[2*nodo+1];
+        int mid = (l + r) / 2;
+        build(arr, 2*node, l, mid);
+        build(arr, 2*node+1, mid+1, r);
+        tree[node] = tree[2*node] + tree[2*node+1];
     }
 
-    void update(int l, int r, int nodo, int pos, int val){
-        if(l==r){
-            arbol[nodo] = val;
+    void push(int node, int l, int r){ // push the lazy updates to the children
+        if(lazy[node] != 0){
+            tree[node] += lazy[node] * (r - l + 1);
+            if(l != r){
+                lazy[2*node] += lazy[node];
+                lazy[2*node+1] += lazy[node];
+            }
+            lazy[node] = 0;
+        }
+    }
+
+    void update(int node, int l, int r, int ql, int qr, int val){
+        push(node, l, r);
+        if(qr < l || ql > r) return;
+        if(ql <= l && r <= qr){
+            lazy[node] = val;
+            push(node, l, r);
             return;
         }
-        int mid = (l+r)/2;
-        if(pos<=mid) update(l, mid, 2*nodo, pos, val);
-        else update(mid+1, r, 2*nodo+1, pos, val);
-        arbol[nodo] = arbol[2*nodo] + arbol[2*nodo+1];
+        int mid = (l + r) / 2;
+        update(2*node, l, mid, ql, qr, val);
+        update(2*node+1, mid+1, r, ql, qr, val);
+        tree[node] = tree[2*node] + tree[2*node+1];
     }
 
-    int query(int l, int r, int nodo, int ql, int qr){
-        if(qr<l || ql>r) return 0;
-        if(ql<=l && r<=qr) return arbol[nodo];
-        int mid = (l+r)/2;
-        return query(l, mid, 2*nodo, ql, qr) + query(mid+1, r, 2*nodo+1, ql, qr);
+    int query(int node, int l, int r, int ql, int qr){
+        push(node, l, r);
+        if(qr < l || ql > r) return 0;
+        if(ql <= l && r <= qr) return tree[node];
+        int mid = (l + r) / 2;
+        return query(2*node, l, mid, ql, qr) + query(2*node+1, mid+1, r, ql, qr);
     }
-}
+};
 
 int main(){
 
     int n,q;
     cin>>n>>q;
     segtree st(n);
-    for(int i=0; i<n; i++) cin>>st.valores[i];
-    st.init(0, n-1, 1);
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) cin>>arr[i];
+    st.build(arr, 1, 0, n-1);
     while(q--){
         int t;
         cin>>t;
         if(t==1){
-            int pos, val;
-            cin>>pos>>val;
-            st.update(0, n-1, 1, pos, val);
+            int l,r,val;
+            cin>>l>>r>>val;
+            st.update(1, 0, n-1, l, r, val);
         }else{
             int l,r;
             cin>>l>>r;
-            cout<<st.query(0, n-1, 1, l, r)<<endl;
+            cout<<st.query(1, 0, n-1, l, r)<<endl;
         }
     }
 
