097eb42619794d4421ce380365d26b7eba73c48c
oscar-alex <oscaralex.qc@gmail.com>
2026-08-02 22:30:28 -0500

    Se hizo una modificacion de el algoritmo de Matrix Exponentation

--- note book/math/matrix exponentation.cpp
+++ note book/math/matrix exponentation.cpp
@@ -1,69 +1,83 @@
 /*
-imagina que quieres calcualr el fibonacci de 4561
-podriamos representar fibo(1) = 1 y fibo(2) = 2
-o tambien como un vector [1 2] y para calcular por el
-tercer termino, podriamos multiplicar por una matriz
-[1 1]
-[1 0]
-entonces tendriamos 
-[1 2] * [1 1] 
-        [1 0]
-lo que nos da [3 1]
-y para el cuarto termino, multiplicamos el resultado por la misma matriz
-[3 1] * [1 1]
-        [1 0]
-para el quinto termino, multiplicamos el resultado por la misma matriz
-[4 3] * [1 1]
-        [1 0]
-podemos ver el patron
-entonces podriamos generalizarlo
-fibo(n) = [fibo(1) fibo(2)] * [1 1]* [1 0] *
-                              [1 0]  [1 0]   ... n-2 veces
-entonces para calcular el fibonacci de 4561, solo tenemos que multiplicar 
-la matriz N veces y usar la tecnica de exponenciacion BINARIA para multiplicar 
-la matriz log(n) veces, lo que nos da un resultado en O(log(n))
+si f(n) = f(n-1) + f(n-2) + 2f(n-3)
+entonces la matriz necesaria seria usar los valores de las constantes
+    1 1 2
+A = 1 0 0
+    0 1 0
+Es decir en tamaño de la funcion dependente de cuantas funciones anteriores es necesario
+entonces si tenemos una matriz base con respecto a los valores base f(0) = a, f(1) = b, f(c) = c
+    a
+B = b
+    c
+entonces f(n) = A**(n-1)*B
+
 */
 
-#define ll long long
-#define MOD 1000000007 
-ll fibonacci(ll n){
-    if(n==1) return 1;
-    if(n==2) return 2;
-    ll F[2][2] = {{1,1},{1,0}};
-    ll res[2][2] = {{1,0},{0,1}}; // Matriz identidad
-    n -= 2; // Ya tenemos los dos primeros terminos
-    while(n>0){
-        if(n&1){ // Si el bit menos significativo es 1
-            // Multiplicamos res por F
-            ll temp[2][2];
-            for(int i=0;i<2;i++){
-                for(int j=0;j<2;j++){
-                    temp[i][j] = (res[i][0]*F[0][j] + res[i][1]*F[1][j]) % MOD;
-                }
-            }
-            for(int i=0;i<2;i++){
-                for(int j=0;j<2;j++){
-                    res[i][j] = temp[i][j];
-                }
-            }
-        }
-        // Elevamos F al cuadrado
-        ll temp[2][2];
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                temp[i][j] = (F[i][0]*F[0][j] + F[i][1]*F[1][j]) % MOD;
-            }
-        }
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                F[i][j] = temp[i][j];
-            }
-        }
-        n>>=1; // Desplazamos el exponente a la derecha para procesar el siguiente bit
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll =  long long;
+using ull = unsigned long long;
+#define endl "\n"
+
+vector<int> dx = {0,0,-1,1};
+vector<int> dy = {-1,1,0,0};
+
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int LG = 20;
+const int N = 2;
+
+vector<vector<ll>> A(N, vector<ll> (N,0));
+vector<vector<ll>> I(N,vector<ll> (N,0));
+vector<vector<ll>> base(N,vector<ll> (N,0));
+
+void start(){
+    for(int i=0; i<N; i++){
+        I[i][i] = 1;
+        A[0][i] = 1;
+        if(i > 0) A[i][i-1] = 1;
+        base[N-i-1][0] = i;        
     }
-    return res[0][0]; // El resultado es el primer elemento de la matriz resultante
 }
 
-/*
-fibonacci de 4561 es 687995182, y se calcula en O(log(4561)).
-*/
+vector<vector<ll>> multi(vector<vector<ll>> A, vector<vector<ll>> B){
+    vector<vector<ll>> res(N,vector<ll> (N,0));
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            for(int k=0; k<N; k++){
+                res[i][j]  = (res[i][j] + (A[i][k]*B[k][j])%MOD)%MOD;
+            }
+        }
+    }
+    return res;
+}
+
+vector<vector<ll>> power(vector<vector<ll>> A, ll e){
+    vector<vector<ll>> ans = I;
+    while(e){
+        if(e&1) ans = multi(ans,A);
+        A = multi(A,A);
+        e >>= 1;
+    }
+    return ans;
+}
+
+ll fibonacci(ll x){
+    if(x == 0) return 0;
+    vector<vector<ll>> fibo = multi(power(A,x-1),base);
+    return fibo[0][0];
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);   
+    cout.tie(0);
+    ll n;
+    cin>>n;
+    start();
+    cout<<fibonacci(n)<<endl;
+
+
+    return 0;
+}
